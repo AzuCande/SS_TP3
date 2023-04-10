1a3ce61abb86afa4b730bfc0e01c4fc366178274
Azul <azkim@itba.edu.ar>
2023-04-10 11:50:57 -0300

    FEATURE: Add file parser with general properties

--- src/main/java/ar/edu/itba/ss/FilesParser.java
+++ src/main/java/ar/edu/itba/ss/FilesParser.java
@@ -1,4 +1,43 @@
 package ar.edu.itba.ss;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class FilesParser {
+    public final static String RESOURCES_PATH = "src/main/resources/";
+    public final static String DYNAMIC_FILE = "dynamic.txt";
+    public final static String STATIC_FILE = "static.txt";
+
+    public static void parseStaticFile() {
+        File staticFile = new File(RESOURCES_PATH + STATIC_FILE);
+        try (Scanner scanner = new Scanner(staticFile)) {
+            if (scanner.hasNextLine()) Utils.tableWidth = Integer.parseInt(scanner.nextLine());
+            if (scanner.hasNextLine()) Utils.tableHeight = Integer.parseInt(scanner.nextLine());
+            if (scanner.hasNextLine()) Utils.particleMass = Integer.parseInt(scanner.nextLine());
+            if (scanner.hasNextLine()) Utils.particleRadius = Integer.parseInt(scanner.nextLine());
+        } catch (Exception e) {
+            System.err.println("Error while parsing static file");
+        }
+    }
+
+    public static Bocha parseDynamicFile() {
+        File dynamicFile = new File(RESOURCES_PATH + DYNAMIC_FILE);
+        Bocha whiteBall = null;
+        try (Scanner scanner = new Scanner(dynamicFile)) {
+            if (scanner.hasNextLine()) {
+                String[] line = scanner.nextLine().split(" ");
+                whiteBall = new Bocha(
+                        Double.parseDouble(line[0]),
+                        Double.parseDouble(line[1]),
+                        Double.parseDouble(line[2]),
+                        Double.parseDouble(line[3]),
+                        Utils.particleRadius,
+                        Utils.particleMass
+                );
+            }
+        } catch (Exception e) {
+            System.err.println("Error while parsing dynamic file");
+        }
+        return whiteBall;
+    }
 }
---
+++ src/main/java/ar/edu/itba/ss/Utils.java
@@ -0,0 +1,8 @@
+package ar.edu.itba.ss;
+
+public class Utils {
+    public static Integer tableWidth;
+    public static Integer tableHeight;
+    public static Integer particleMass;
+    public static Integer particleRadius;
+}
